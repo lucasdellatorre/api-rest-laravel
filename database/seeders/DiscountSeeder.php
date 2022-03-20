9b560d1dd227f18119a504b58415576ace652aad
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-03-20 19:52:31 -0300

    adicionando o seeder de Discount

---
+++ database/seeders/DiscountSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DiscountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $discounts =
+        [
+            [
+                'percentual' => 10,
+                'category_id' => 1
+            ],
+            [
+                'percentual' => 5,
+                'category_id' => 2
+            ],
+            [
+                'percentual' => 12,
+                'category_id' => 3
+            ],
+
+        ]
+
+        foreach($discounts as $discount)
+        {
+            Discount::create($discount);
+        }
+    }
+}
