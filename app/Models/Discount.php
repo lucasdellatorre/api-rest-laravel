2f4aea5acb64cb3e2d8054dc138a9a52cb34f788
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-03-20 17:38:39 -0300

    Adicionando a primeira versao + models iniciais

---
+++ app/Models/Discount.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Discount extends Model
+{
+    use HasFactory;
+
+    protected $table = 'discount_category_rules'
+
+    protected $fillable = [
+        'percentual',
+        'category_id'
+    ];
+}
