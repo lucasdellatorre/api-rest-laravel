19bc2834f76b3aad2bfa015b2a8a2b9085b349a7
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-03-20 19:37:31 -0300

    adicionando a migration de discounts

---
+++ database/migrations/2022_03_20_223144_create_discounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discounts_categories_rules', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('percentual', 5, 2);
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+
+            $table->foreign('category_id')
+            ->references('id')
+            ->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discounts');
+    }
+};
