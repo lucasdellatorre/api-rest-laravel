22665f474441b09c1deee691ae186418c3e6dc90
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-03-20 19:45:23 -0300

    adicionando o seeder de Category

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = 
+        [
+            [
+                'id' => 1,
+                'name' => 'Roupas'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Eletrodomesticos'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Celulares'
+            ]
+        ]
+
+        foreach ($categories as $category)
+        {
+            Category::create($category)
+        }
+        
+    }
+}
