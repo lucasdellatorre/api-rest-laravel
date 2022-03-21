fe3b8dcdf3bd1fc1737e202df92df7c3be9dab95
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-03-20 23:13:54 -0300

    modificando a tabela de discounts

--- database/migrations/2022_03_20_223144_create_discounts_table.php
+++ database/migrations/2022_03_20_223144_create_discounts_table.php
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('discounts');
+        Schema::dropIfExists('discounts_categories_rules');
     }
 };
